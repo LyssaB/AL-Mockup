21d1d99cafbc7602050777ba2776901a6f6cfdb3
LyssaB <melissadawn189@gmail.com>
2018-01-20 13:20:53 -0500

    Admin views

---
+++ app/controllers/admin_views_controller.rb
@@ -0,0 +1,13 @@
+class AdminViewsController < ApplicationController
+  def dashboard
+  end
+
+  def assign
+  end
+
+  def scores
+  end
+
+  def users
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'admin_views/dashboard'
+
+  get 'admin_views/assign'
+
+  get 'admin_views/scores'
+
+  get 'admin_views/users'
+
+  get 'admin_views/index'
+  root 'admin_views#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
