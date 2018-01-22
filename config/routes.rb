5223438d7d1cf2c6dae39c403ce162b70cdb8882
LyssaB <melissadawn189@gmail.com>
2018-01-21 21:59:15 -0500

    routes file

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,15 @@ Rails.application.routes.draw do
   get 'admin_views/users'
 
   get 'admin_views/index'
+
+  get 'sample_pages/assign'
+
+  get 'sample_pages/customize'
+
+  get 'sample_pages/scores'
+
+  get 'sample_pages/users'
+
   root 'admin_views#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
