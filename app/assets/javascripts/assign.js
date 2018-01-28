6f59b51b03f0eed29d2829be7dfc13977165ad4b
LyssaB <melissadawn189@gmail.com>
2018-01-28 15:35:15 -0500

    testing js

--- app/assets/javascripts/assign.js
+++ app/assets/javascripts/assign.js
@@ -2,4 +2,12 @@ $(document).on('turbolinks:load', function(){
 	$("#CNA-assign").click(function(){
 		$("#assign").show();
 	});
+
+	$("#dashview1").click(function(){
+		$("#assessmentNotTaken").toggle();
+	});
+
+	$("#dashview2").click(function(){
+		$("#assessmentTaken").toggle();
+	});
 });
--- app/assets/javascripts/learnerViews.js
+++
@@ -1,9 +0,0 @@
-$(document).on('turbolinks:load', function(){
-	$("#dashview1").click(function(){
-		$("#assessmentNotTaken").toggle();
-	});
-
-	$("#dashview2").click(function(){
-		$("#assessmentTaken").toggle();
-	});
-});
